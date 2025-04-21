5e1e6fa6c2b75f9b7136f7a67cca044adc813364
CharlesKibet <charlesbiegon973@gmail.com>
2025-04-21 14:43:54 +0300

    removed google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/sonner"
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Safe Key - Password Generator & Manage",
@@ -25,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Toaster richColors />
         {children}
       </body>
